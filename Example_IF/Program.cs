0e30a398d1db4955e7196a8a8ad4e575f7d82a59
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-09-28 17:40:48 +0700

    IF_ELSE

---
+++ Example_IF/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}");
+}
---
+++ Example_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = new Random().Next(1,10); // целое число от 1 от 9
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine($"{numberA} + {numberB} = {result}");
