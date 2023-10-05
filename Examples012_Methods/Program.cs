c938dfa00ebda835a1dbd2d993613aa3b5280d32
Dmitriy_Pakhomov <pahomov_dmitrii@mail.ru>
2023-10-05 22:48:10 +0700

    Examples012 added

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Методы
 
-//Вид 1
+//Вид 1 (ничего не принимает, ничего не передаёт)
 void Method1()
 {
     Console.WriteLine("Автор Пахомов Дмитрий");
@@ -9,14 +9,16 @@ void Method1()
 Method1();
 System.Console.WriteLine();
 
-//Вид 2
+//Вид 2 (что-то принимает, ничего не передаёт)
 void Method2(string msg)
 {
     System.Console.WriteLine(msg);
 }
+
 Method2("Текст сообщения");
 System.Console.WriteLine();
 
+// метод с несколькими параметрами
 void Method21(string msg, int count)
 {
     int i = 0;
@@ -27,12 +29,13 @@ void Method21(string msg, int count)
     }
     System.Console.WriteLine(msg);
 }
+
 Method21(count: 4, msg: "Текст сообщения 2");
 System.Console.WriteLine();
 System.Console.WriteLine();
 
 
-//Вид 3
+//Вид 3  (ничего не принимает, что-то передаёт)
 int Method3()
 {
     return DateTime.Now.Year;
@@ -43,12 +46,12 @@ Console.WriteLine(year);
 System.Console.WriteLine();
 
 
-//Вид 4
+//Вид 4 (что-то принимает, что-то передаёт)
 string Method4(int count, string text)
 {
     int i = 0;
     string result = String.Empty;
-    while(i < count)
+    while (i < count)
     {
         result = result + text;
         i++;
@@ -60,3 +63,32 @@ string res = Method4(10, "ZV_");
 System.Console.WriteLine(res);
 System.Console.WriteLine();
 
+// ======== Работа с текстом==========
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123
+// s[3] - это r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+System.Console.WriteLine(text);
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
